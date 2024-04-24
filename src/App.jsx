650771576cf07f424512d0ac41e62202fd89b72b
Marco <marcorosas1008@gmail.com>
2024-04-24 17:56:27 -0500

    Move & Stop Pointer
    Add follow move and stop pointer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
-
+import {useEffect, useState} from "react"
 
 function App() {
 
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({x:0, y:0})
+
+  useEffect(() => {
+    
+    const handleMove = (event) => {
+      const {clientX, clientY} = event
+      setPosition({x: clientX, y: clientY})
+    }
+
+    //ejecutar solo cuando el boton este activado
+    if(enabled){
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    //se ejecuta cuando se desmonte el componente
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+
+  },[enabled])
 
   return (
     <>
     <h3>Mouse Follower</h3>
+
+    <div style={
+      {
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: '0.8',
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}0px,${position.y}0px)`
+      }
+    }></div>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'}
+        Activar seguir puntero
+      </button>
     </>
   )
 }
